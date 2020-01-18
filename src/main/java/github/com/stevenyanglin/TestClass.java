2bdad513d68d1c5c4cf75dbc13f22ea21a39db42
stevenyanglin <stevenyl@163.com>
2020-01-18 15:40:38 +0800

    commit a TestClass.java file

---
+++ src/main/java/github/com/stevenyanglin/TestClass.java
@@ -0,0 +1,11 @@
+package github.com.stevenyanglin;
+
+/**
+ * @ Author     ：linyang3.
+ * @ Date       ：Created in 15:39 2020/1/18
+ * @ Description：${description}
+ * @ Modified By：
+ * @Version: $version$
+ */
+public class TestClass {
+}
