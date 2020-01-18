e927cfa431995c50f152e9f96e721ef0580e288b
unknown <stevenyl@163.com>
2020-01-18 15:30:56 +0800

    1.test 2.why

---
+++ src/main/java/github/com/stevenyanglin/App.java
@@ -0,0 +1,13 @@
+package github.com.stevenyanglin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
